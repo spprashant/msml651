d5c66f6ca7fd9d708d412fbebbc5d466f5e81614
Prashant Swarnapuri <prashrao91@gmail.com>
2021-12-05 12:52:49 -0500

    Update: README.md notebooks

--- notebooks/Users/pswarnap@umd.edu/start_ml.py
+++ notebooks/Users/pswarnap@umd.edu/start_ml.py
@@ -19,14 +19,18 @@ from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import RegressionEvaluator
 
 from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.regression import RandomForestRegressionModel
 from pyspark.ml.regression import LinearRegression
+from pyspark.ml.regression import LinearRegressionModel
 from pyspark.ml.regression import GBTRegressor
+from pyspark.ml.regression import GBTRegressionModel
 
 import mlflow
 import mlflow.spark
 
 import pandas as pd
 import seaborn as sns
+import matplotlib.pylab as plt
 
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 
@@ -106,28 +110,33 @@ data_df = spark.read.format("csv") \
 # COMMAND ----------
 
 # sample a portion for dev purposes (set fraction=1.00 for full dataset run)
-trips_df = data_df.sample(fraction=0.01, seed=42)
+trips_df = data_df.sample(fraction=1.00, seed=42)
 
 # COMMAND ----------
 
-## Remove trips with invalid coordinates
-# Filter by extents of NYC
-MIN_LAT = 39.97291055131899
-MAX_LAT = 41.84501267270689
-MIN_LON = -74.8828125
-MAX_LON = -71.510009765625
-
-trips_df = trips_df.filter((trips_df["pickup_latitude"] <= MAX_LAT) & (trips_df["pickup_latitude"] >= MIN_LAT))
-trips_df = trips_df.filter((trips_df["pickup_longitude"] <= MAX_LON) & (trips_df["pickup_longitude"] >= MIN_LON))
-trips_df = trips_df.filter((trips_df["dropoff_latitude"] <= MAX_LAT) & (trips_df["dropoff_latitude"] >= MIN_LAT))
-trips_df = trips_df.filter((trips_df["dropoff_longitude"] <= MAX_LON) & (trips_df["dropoff_longitude"] >= MIN_LON))
-trips_df = trips_df.filter((trips_df["fare_amount"] > 2) & (trips_df["fare_amount"] < 500))
-trips_df = trips_df.filter((trips_df["passenger_count"] > 0) & (trips_df["passenger_count"]  < 6))
+display(trips_df.select("pickup_latitude", "pickup_longitude", "dropoff_latitude", "dropoff_longitude").describe())
 
 # COMMAND ----------
+
+# UDF function to check if coordinate is near airport
+def is_near_airport(lat, lon):
+    JFK_LAT, JFK_LON= 40.639722, -73.778889
+    EWR_LAT, EWR_LON= 40.6925, -74.168611
+    LGA_LAT, LGA_LON= 40.77725, -73.872611
+
+    if lat is None or lon is None:
+        return False
+
+    jfk_dist = haversine_distance(lat, lon, F.lit(JFK_LAT), F.lit(JFK_LON))
+    ewr_dist = haversine_distance(lat, lon, F.lit(EWR_LAT), F.lit(EWR_LON))
+    lga_dist = haversine_distance(lat, lon, F.lit(LGA_LAT), F.lit(LGA_LON))
+
+
+    return F.when((jfk_dist < 1) | (ewr_dist < 1) | (lga_dist < 1),True).otherwise(False)
+
+spark.udf.register("is_near_airport", is_near_airport, BooleanType())
 
 ## Function to calculate the haversine distance from start to end coordinates
-
 def haversine_distance(start_lat, start_lon, end_lat, end_lon):
     start_lat, start_lon, end_lat, end_lon = map(F.radians,  [start_lat, start_lon, end_lat, end_lon])
     a = F.sin((end_lon - start_lon)/2) ** 2 + F.cos(start_lat) * F.cos(end_lat) * F.sin((end_lat - start_lat)/2) ** 2
@@ -137,80 +146,98 @@ def haversine_distance(start_lat, start_lon, end_lat, end_lon):
 
 spark.udf.register("haversine_distance", haversine_distance, DoubleType())
 
-
-## calculate haversine distance from start to end of trip
-trips_df = trips_df.withColumn("distance",haversine_distance(trips_df["pickup_latitude"],trips_df["pickup_longitude"],trips_df["dropoff_latitude"],trips_df["dropoff_longitude"]))
-
 hour = F.udf(lambda x: x.hour, IntegerType())
 month = F.udf(lambda x: x.month, IntegerType())
 year = F.udf(lambda x: x.year, IntegerType())
 dow = F.udf(lambda x: x.weekday(), IntegerType())
 day = F.udf(lambda x: x.day, IntegerType())
 
+# COMMAND ----------
 
-## Get local time in New York
-trips_df = trips_df.withColumn("localtime", F.from_utc_timestamp(trips_df["pickup_datetime"],"America/New_York"))
+## Remove trips with invalid coordinates
+# Filter by extents of NYC
+def filter_bad_data(trips_df, is_validation=False):
+    MIN_LAT = 39.97291055131899
+    MAX_LAT = 41.84501267270689
+    MIN_LON = -74.8828125
+    MAX_LON = -71.510009765625
 
-##Pickup time features in New York
-trips_df = trips_df.withColumn("pickup_year", year(trips_df["localtime"]))
-trips_df = trips_df.withColumn("pickup_month", month(trips_df["localtime"]))
-trips_df = trips_df.withColumn("pickup_day", day(trips_df["localtime"]))
-trips_df = trips_df.withColumn("pickup_hour", hour(trips_df["localtime"]))
-trips_df = trips_df.withColumn("pickup_dow", dow(trips_df["localtime"]))
-
+    trips_df = trips_df.filter((trips_df["pickup_latitude"] <= MAX_LAT) & (trips_df["pickup_latitude"] >= MIN_LAT))
+    trips_df = trips_df.filter((trips_df["pickup_longitude"] <= MAX_LON) & (trips_df["pickup_longitude"] >= MIN_LON))
+    trips_df = trips_df.filter((trips_df["dropoff_latitude"] <= MAX_LAT) & (trips_df["dropoff_latitude"] >= MIN_LAT))
+    trips_df = trips_df.filter((trips_df["dropoff_longitude"] <= MAX_LON) & (trips_df["dropoff_longitude"] >= MIN_LON))
+    if not is_validation:
+        trips_df = trips_df.filter((trips_df["fare_amount"] > 2) & (trips_df["fare_amount"] < 500))
+    trips_df = trips_df.filter((trips_df["passenger_count"] > 0) & (trips_df["passenger_count"]  < 6))
+    return trips_df
 
 # COMMAND ----------
 
-##Peak Time (monday to friday 6-10AM and 16-20PM)
-trips_df = trips_df.withColumn('is_peaktime', F.when((((trips_df['pickup_hour'] >= 16) & (trips_df['pickup_hour'] < 20)) | ((trips_df['pickup_hour'] >= 6 ) & (trips_df['pickup_hour']< 10 ))) &  ((trips_df['pickup_dow']!=6) & (trips_df['pickup_dow']!=0)), True).otherwise(False))
-
-##Night Time (All days 10PM to 6AM)
-trips_df = trips_df.withColumn('is_nighttime', F.when(((trips_df['pickup_hour'] >= 22) & (trips_df['pickup_hour'] < 6)),True).otherwise(False))
-
-
-# Needed for join with weather data (both UTC)
-trips_df = trips_df.withColumn("utc_hour",  F.date_format(trips_df['pickup_datetime'], "H").cast(IntegerType()))
-trips_df = trips_df.withColumn("utc_year",  F.date_format(trips_df['pickup_datetime'], "yyyy").cast(IntegerType()))
-trips_df = trips_df.withColumn("utc_month",  F.date_format(trips_df['pickup_datetime'], "M").cast(IntegerType()))
-trips_df = trips_df.withColumn("utc_dow",  F.date_format(trips_df['pickup_datetime'], "F").cast(IntegerType()))
-trips_df = trips_df.withColumn("utc_day",  F.date_format(trips_df['pickup_datetime'], "d").cast(IntegerType()))
-
+def generate_distance_feature(trips_df):
+    ## calculate haversine distance from start to end of trip
+    trips_df = trips_df.withColumn("distance",haversine_distance(trips_df["pickup_latitude"],trips_df["pickup_longitude"],trips_df["dropoff_latitude"],trips_df["dropoff_longitude"]))
+    return trips_df
 
 # COMMAND ----------
 
-# UDF function to check if coordinate is near airport
-def is_near_airport(lat, lon):
-    JFK_LAT, JFK_LON= 40.639722, -73.778889
-    EWR_LAT, EWR_LON= 40.6925, -74.168611
-    LGA_LAT, LGA_LON= 40.77725, -73.872611
-    
-    if lat is None or lon is None:
-        return False
-    
-    jfk_dist = haversine_distance(lat, lon, F.lit(JFK_LAT), F.lit(JFK_LON))
-    ewr_dist = haversine_distance(lat, lon, F.lit(EWR_LAT), F.lit(EWR_LON))
-    lga_dist = haversine_distance(lat, lon, F.lit(LGA_LAT), F.lit(LGA_LON))
-    
-    
-    return F.when((jfk_dist < 1) | (ewr_dist < 1) | (lga_dist < 1),True).otherwise(False)
+def generate_datetime_features(trips_df):
 
-spark.udf.register("is_near_airport", is_near_airport, BooleanType())
+    ## Get local time in New York
+    trips_df = trips_df.withColumn("localtime", F.from_utc_timestamp(trips_df["pickup_datetime"],"America/New_York"))
 
-## Calculate flag to indicate if pickup or dropoff is near airport
-trips_df = trips_df.withColumn("is_pickup_near_airport", is_near_airport(trips_df["pickup_latitude"], trips_df["pickup_longitude"]))
-trips_df = trips_df.withColumn("is_dropoff_near_airport", is_near_airport(trips_df["dropoff_latitude"], trips_df["dropoff_longitude"]))
+    ##Pickup time features in New York
+    trips_df = trips_df.withColumn("pickup_year", year(trips_df["localtime"]))
+    trips_df = trips_df.withColumn("pickup_month", month(trips_df["localtime"]))
+    trips_df = trips_df.withColumn("pickup_day", day(trips_df["localtime"]))
+    trips_df = trips_df.withColumn("pickup_hour", hour(trips_df["localtime"]))
+    trips_df = trips_df.withColumn("pickup_dow", dow(trips_df["localtime"]))
+
+    ##Peak Time (monday to friday 6-10AM and 16-20PM)
+    trips_df = trips_df.withColumn('is_peaktime', F.when((((trips_df['pickup_hour'] >= 16) & (trips_df['pickup_hour'] < 20)) | ((trips_df['pickup_hour'] >= 6 ) & (trips_df['pickup_hour']< 10 ))) &  ((trips_df['pickup_dow']!=6) & (trips_df['pickup_dow']!=0)), True).otherwise(False))
+
+    ##Night Time (All days 10PM to 6AM)
+    trips_df = trips_df.withColumn('is_nighttime', F.when(((trips_df['pickup_hour'] >= 22) & (trips_df['pickup_hour'] < 6)),True).otherwise(False))
+
+
+    # Needed for join with weather data (both UTC)
+    trips_df = trips_df.withColumn("utc_hour",  F.date_format(trips_df['pickup_datetime'], "H").cast(IntegerType()))
+    trips_df = trips_df.withColumn("utc_year",  F.date_format(trips_df['pickup_datetime'], "yyyy").cast(IntegerType()))
+    trips_df = trips_df.withColumn("utc_month",  F.date_format(trips_df['pickup_datetime'], "M").cast(IntegerType()))
+    trips_df = trips_df.withColumn("utc_dow",  F.date_format(trips_df['pickup_datetime'], "F").cast(IntegerType()))
+    trips_df = trips_df.withColumn("utc_day",  F.date_format(trips_df['pickup_datetime'], "d").cast(IntegerType()))
+    return trips_df
 
 # COMMAND ----------
 
-## Join trips data with LGA and NYC weather station data
+def generate_poi_features(trips_df):
 
-trips_df = trips_df.join(lga_weather_df,    
-              [trips_df[f] == lga_weather_df[s] for (f, s) in zip(["utc_year", "utc_month", "utc_day", "utc_hour"], ["lga_year" , "lga_month", "lga_day", "lga_hour"])], 
-   "inner")
+    ## Calculate flag to indicate if pickup or dropoff is near airport
+    trips_df = trips_df.withColumn("is_pickup_near_airport", is_near_airport(trips_df["pickup_latitude"], trips_df["pickup_longitude"]))
+    trips_df = trips_df.withColumn("is_dropoff_near_airport", is_near_airport(trips_df["dropoff_latitude"], trips_df["dropoff_longitude"]))
+    return trips_df
 
-trips_df = trips_df.join(nyc_weather_df,    
-              [trips_df[f] == nyc_weather_df[s] for (f, s) in zip(["utc_year", "utc_month", "utc_day", "utc_hour"], ["nyc_year" , "nyc_month", "nyc_day", "nyc_hour"])], 
-   "inner")
+# COMMAND ----------
+
+def append_weather_features(trips_df):
+    ## Join trips data with LGA and NYC weather station data
+
+    trips_df = trips_df.join(lga_weather_df,    
+                  [trips_df[f] == lga_weather_df[s] for (f, s) in zip(["utc_year", "utc_month", "utc_day", "utc_hour"], ["lga_year" , "lga_month", "lga_day", "lga_hour"])], 
+       "inner")
+
+    trips_df = trips_df.join(nyc_weather_df,    
+                  [trips_df[f] == nyc_weather_df[s] for (f, s) in zip(["utc_year", "utc_month", "utc_day", "utc_hour"], ["nyc_year" , "nyc_month", "nyc_day", "nyc_hour"])], 
+       "inner")
+    return trips_df
+
+# COMMAND ----------
+
+## Data set preparation
+trips_df = filter_bad_data(trips_df)
+trips_df = generate_distance_feature(trips_df)
+trips_df = generate_datetime_features(trips_df)
+trips_df = generate_poi_features(trips_df)
+trips_df = append_weather_features(trips_df)
 
 # COMMAND ----------
 
@@ -228,7 +255,23 @@ sns.scatterplot(data=pandas_df, x="fare_amount", y="distance")
 
 # COMMAND ----------
 
+pandas_df = trips_df.select("fare_amount","is_pickup_near_airport").toPandas()
 
+sns.distplot(pandas_df[(pandas_df["is_pickup_near_airport"] == True) & (pandas_df["fare_amount"] < 100)]["fare_amount"], label="Airport Pickups")
+sns.distplot(pandas_df[(pandas_df["is_pickup_near_airport"] == False) & (pandas_df["fare_amount"] < 100)]["fare_amount"], label="Non-airport Pickups")
+
+plt.legend()
+plt.show()
+
+# COMMAND ----------
+
+pandas_df = trips_df.select("fare_amount","is_dropoff_near_airport").toPandas()
+
+sns.distplot(pandas_df[(pandas_df["is_dropoff_near_airport"] == True) & (pandas_df["fare_amount"] < 100)]["fare_amount"], label="Airport Dropoffs")
+sns.distplot(pandas_df[(pandas_df["is_dropoff_near_airport"] == False) & (pandas_df["fare_amount"] < 100)]["fare_amount"], label="Non-airport Dropoffs")
+
+plt.legend()
+plt.show()
 
 # COMMAND ----------
 
@@ -237,9 +280,10 @@ sns.scatterplot(data=pandas_df, x="fare_amount", y="distance")
 (train_df, test_df) = trips_df.randomSplit([0.8, 0.2], seed=42)
 
 ## Select features and create assembler
-variables = ["pickup_latitude", "pickup_longitude", "dropoff_latitude", "dropoff_longitude", "distance" , "is_peaktime", "is_nighttime", "is_pickup_near_airport", "is_dropoff_near_airport", "pickup_month","pickup_year","pickup_dow", "passenger_count","nyc_is_precip","nyc_is_ice", "nyc_is_snow", "lga_is_precip","lga_is_ice", "lga_is_snow"]
+#variables = ["pickup_latitude", "pickup_longitude", "dropoff_latitude", "dropoff_longitude", "distance" , "is_peaktime", "is_nighttime", "is_pickup_near_airport", "is_dropoff_near_airport", "pickup_month","pickup_year","pickup_dow", "passenger_count","nyc_is_precip","nyc_is_ice", "nyc_is_snow", "lga_is_precip","lga_is_ice", "lga_is_snow"]
 
-#, "is_peaktime", "is_nighttime", "is_pickup_near_airport", "is_dropoff_near_airport", "pickup_month","pickup_year","pickup_dow", "passenger_count","nyc_is_precip","nyc_is_ice", "nyc_is_snow", "lga_is_precip","lga_is_ice", "lga_is_snow"
+## Baseline features
+variables = ["pickup_latitude", "pickup_longitude", "dropoff_latitude", "dropoff_longitude", "distance"]
 
 assembler = VectorAssembler(
     inputCols=variables,
@@ -385,7 +429,7 @@ with mlflow.start_run():
 
 with mlflow.start_run():
 
-    REG_PARAM = 0.3
+    REG_PARAM = 0
     ELASTIC_NET_PARAM = 0 
     
     lr = LinearRegression(featuresCol='features', labelCol="fare_amount", regParam = REG_PARAM, elasticNetParam = ELASTIC_NET_PARAM, solver = 'auto')
@@ -404,6 +448,8 @@ with mlflow.start_run():
     mlflow.log_param('model','LR_single_run')
     mlflow.log_param('REG_PARAM-ELASTIC_NET_PARAM' , str([REG_PARAM, ELASTIC_NET_PARAM]))
     mlflow.log_metric('test_rmse', rmse)
+    lr_model.write().overwrite().save('/tmp/lr_model')
+    
 
     print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
 
@@ -429,6 +475,7 @@ with mlflow.start_run():
     mlflow.log_param('model','RF_single_run')
     mlflow.log_param('MAX_DEPTH-NUM_TREES' , str([MAX_DEPTH, NUM_TREES]))
     mlflow.log_metric('test_rmse', rmse)
+    rf_model.write().overwrite().save('/tmp/rf_model')
 
     print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
 
@@ -437,15 +484,12 @@ with mlflow.start_run():
 
 ## Single run Gradient Boosted Trees
 with mlflow.start_run():
-    MAX_DEPTH = 10
-    MAX_BINS = 20 
+    MAX_DEPTH = 5
+    MAX_BINS = 32 
 
     gb = GBTRegressor(featuresCol='features', labelCol="fare_amount",  maxBins=MAX_BINS, maxDepth=MAX_DEPTH)
 
     gb_model = gb.fit(train_df)
-
-
-    from pyspark.ml.evaluation import RegressionEvaluator
 
     predictions = gb_model.transform(test_df)
 
@@ -455,5 +499,6 @@ with mlflow.start_run():
     mlflow.log_param('model','GB_single_run')
     mlflow.log_param('MAX_DEPTH-MAX_BINS' , str([MAX_DEPTH, MAX_BINS]))
     mlflow.log_metric('test_rmse', rmse)
+    gb_model.write().overwrite().save('/tmp/gb_model')
 
     print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
